e2768b8e7bee8736934d69351612e5ec73e5fc9f
Uyen Mai Nguyen <uyen.nguyenmai@politetech.com>
2025-02-13 10:50:38 +0700

    Bai tap 1- Playwright- Lesson 1

---
+++ tests/example.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://material.playwrightvn.com/');
+  await expect(page.getByText('Tài liệu học automation test')).toBeVisible();
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://material.playwrightvn.com/');
+  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
+  await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+});
