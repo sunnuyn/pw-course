4bc5de44572811ebe7af715b8072238f8fe39fb7
PW - Uyen <uyenntm.r1@gmail.com>
2025-02-17 11:10:30 +0700

    Bai tap 2: Javascript

---
+++ tests/lesson-2/ex4.js
@@ -0,0 +1,11 @@
+let myHeight = 1.60;
+
+canNangLyTuong = 60 * 9 / 10;
+canNangToiDa = 60;
+canNangToiThieu = 60 * 8 / 10;
+
+console.log(
+  "Cân nặng lý tưởng" + canNangLyTuong, 
+  "Cân nặng tối đa: " + canNangToiDa, 
+  "Cân nặng tối thiểu:" + canNangToiThieu
+);
